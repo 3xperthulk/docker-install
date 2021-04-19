a68dbd7a8082684356e8457ef79537f42a3bf6ae
3xperthulk <41061831+3xperthulk@users.noreply.github.com>
2021-04-19 12:26:23 +0530

    Create loganalyticexport.ps1

---
+++ loganalyticexport.ps1
@@ -0,0 +1,65 @@
+Function Get-LogAnalyticsSignature {
+    [cmdletbinding()]
+    Param (
+        $customerId,
+        $sharedKey,
+        $date,
+        $contentLength,
+        $method,
+        $contentType,
+        $resource
+    )
+    $xHeaders = "x-ms-date:" + $date
+    $stringToHash = $method + "`n" + $contentLength + "`n" + $contentType + "`n" + $xHeaders + "`n" + $resource
+
+    $bytesToHash = [Text.Encoding]::UTF8.GetBytes($stringToHash)
+    $keyBytes = [Convert]::FromBase64String($sharedKey)
+
+    $sha256 = New-Object System.Security.Cryptography.HMACSHA256
+    $sha256.Key = $keyBytes
+    $calculatedHash = $sha256.ComputeHash($bytesToHash)
+    $encodedHash = [Convert]::ToBase64String($calculatedHash)
+    $authorization = 'SharedKey {0}:{1}' -f $customerId,$encodedHash
+    return $authorization
+}
+
+Function Export-LogAnalytics {
+    [cmdletbinding()]
+    Param(
+        $customerId,
+        $sharedKey,
+        $object,
+        $logType
+    )
+    $bodyAsJson = ConvertTo-Json $object
+    $body = [System.Text.Encoding]::UTF8.GetBytes($bodyAsJson)
+
+    $method = "POST"
+    $contentType = "application/json"
+    $resource = "/api/logs"
+    $rfc1123date = [DateTime]::UtcNow.ToString("r")
+    $contentLength = $body.Length
+
+    $signatureArguments = @{
+        CustomerId = $customerId
+        SharedKey = $sharedKey
+        Date = $rfc1123date
+        ContentLength = $contentLength
+        Method = $method
+        ContentType = $contentType
+        Resource = $resource
+    }
+
+    $signature = Get-LogAnalyticsSignature @signatureArguments
+    
+    $uri = "https://" + $customerId + ".ods.opinsights.azure.com" + $resource + "?api-version=2016-04-01"
+
+    $headers = @{
+        "Authorization" = $signature;
+        "Log-Type" = $logType;
+        "x-ms-date" = $rfc1123date;
+    }
+
+    $response = Invoke-WebRequest -Uri $uri -Method $method -ContentType $contentType -Headers $headers -Body $body -UseBasicParsing
+    return $response.StatusCode
+}
